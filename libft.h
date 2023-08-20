3778359bcc270449d16b4cc7afd35c3307494ca4
brayans22 <bsaiago@fi.uba.ar>
2023-08-20 21:34:04 +0200

    ft_putnbr_fd.c

--- libft.h
+++ libft.h
@@ -41,6 +41,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void 	ft_putchar_fd(char c, int fd);
 void    ft_putstr_fd(char *s, int fd);
 void 	ft_putendl_fd(char *s, int fd);
+void 	ft_putnbr_fd(int nb, int fd);
 
 /* Bonus */
 typedef struct		s_list
