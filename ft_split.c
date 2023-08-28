8b8e97877379ba8e64b7b56004f523cf75855a19
brayans22 <bsaiago@fi.uba.ar>
2023-08-28 11:41:51 +0200

    fix split

--- ft_split.c
+++ ft_split.c
@@ -1,125 +1,90 @@
 #include "libft.h"
 
-char    *ft_strcpy(char *dest, const char *src)
+char    **get_new_split(char **old_split, char *sub, unsigned int count_strings)
 {
-	size_t i;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-void    free_split_memory(char **split, unsigned int count_strings)
-{
-    unsigned int    i;
-
-    i = 0;
-    while (i < count_strings)
-    {
-        free(split[i]);
-        i++;
-    }
-    free(split);
-}
-
-char    **get_new_split(char **split, char *sub, unsigned int count_strings)
-{
-    unsigned int    i;
-    char            **new_split; 
-
-    i = -1;
-    new_split = (char **)malloc(sizeof(char *) * count_strings);
-    if (!new_split)
-    {
-        free_split_memory(split, count_strings);
+    int             i;
+    char            **new_split;
+    
+    new_split = (char **) malloc(sizeof(char *) * (count_strings + 1));
+    if (!new_split || !sub)
         return (NULL);
-    }
-    while (++i < count_strings - 1)
+    i = -1;
+    while (++i < (int)count_strings - 1)
     {
-        new_split[i] = (char *)malloc(ft_strlen(split[i]) + 1);
+        new_split[i] = ft_strdup(old_split[i]);
         if (!new_split[i])
         {
-            free_split_memory(new_split, i - 1);
             return (NULL);
         }
-        ft_strcpy(new_split[i], split[i]);
     }
-    free_split_memory(split, count_strings - 1);
-    new_split[i] = (char *)malloc(ft_strlen(sub) + 1);
-    if (!new_split[i])
-    {
-        free_split_memory(new_split, count_strings - 1);
-        return (NULL);
-    }   
-    ft_strcpy(new_split[i], sub);
+    new_split[i++] = ft_strdup(sub);
+    new_split[i] = NULL;
     return (new_split);
 }
 
 char    **ft_split(char const *s, char c)
 {
-    char            *sub;
     char            **split;
-    unsigned int    index_start_substr;
+    unsigned int    len;
+    unsigned int    start;
     unsigned int    i;
     unsigned int    count_strings;
 
-    if(!s)
-        return (NULL);
     i = 0;
-    index_start_substr = 0;
     count_strings = 0;
     while (s[i])
-    {    
-        if ((s[i] == c) || (index_start_substr != 0 && s[i + 1] == '\0'))
+    {
+        len = 0;
+        start = 0;
+        while (s[i] == c && s[i])
         {
-            if (index_start_substr == 0)
-                sub = ft_substr(s, index_start_substr + 1, i);
-            else
-                sub = ft_substr(s, index_start_substr + 1, i - index_start_substr - 1);
-            index_start_substr = i;
-            if (!sub)
-            {
-                free_split_memory(split, count_strings);
-                return (NULL);
-            }
-            count_strings++;
-                      if (count_strings == 1)
-            {
-                split = (char **)malloc(sizeof(char *) * count_strings);
-                split[0] = (char *)malloc(ft_strlen(sub) + 1);
-            }
-            
-            spl
-  it = get_new_split(split, sub, count_strings);
-            free(sub);
-            if (!split)
-                return (NULL);
-            
+            if (s[i] == '\0' && count_strings == 0)
+                return (get_new_split(split, ft_substr(s, start, len), count_strings));
+            i++;
         }
-        i++;
+        start = i;
+        while (s[i] != c && s[i])
+        {
+            len++;
+            i++;
+        }
+        if (len > 0)
+        {
+            count_strings++;
+            split = get_new_split(split, ft_substr(s, start, len), count_strings);
+        }
+        else 
+            return (NULL);
     }
-    if (index_start_substr == 0)
-        return (get_new_split(split, ft_substr(s, index_start_substr, i), count_strings + 1));
     return (split);
 }
 
+/*
 #include <stdio.h>
 #include <string.h>
 int main()
 {
-    //char *v = "          ";
-    char *seg = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse";
-
-    char **split = ft_split(seg, ' ');
-
-    for (int i = 0; i < 12; i++)
+    char *seg =    "juan jose lopez";
+    char **split_copy = ft_split(seg, 'w');
+    
+    int i = 0;
+    while (split_copy[i] != NULL)
     {
-        printf("%s\n", split[i]);
+        printf("%s\n", split_copy[i]);
+        i++;
+    }
+    printf("-----------------------------------------------------------------------------------------------\n");
+
+    char cadena[] = "juan jose lopez";
+    char delimitador[] = "w";
+    char *token = strtok(cadena, delimitador);
+    if(token != NULL){
+        while(token != NULL)
+        {
+            printf("%s\n", token);
+            token = strtok(NULL, delimitador);
+        }
     }
     return 0;
 }
+*/
